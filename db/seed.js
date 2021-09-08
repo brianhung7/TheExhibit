4b0e5ff7912895e81a34c3eaa385aa4bcb933dfd
brianhung7 <brian.texts@gmail.com>
2021-09-07 22:01:38 -0700

    Seed file started

---
+++ db/seed.js
@@ -0,0 +1,10 @@
+const { ObjectId } = require('mongodb');
+const Comment = require("../models/Comment");
+const Like = require("../models/Like");
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+//https://imgur.com/gallery/jfPXb
+//https://imgur.com/gallery/DDtiz
+//https://imgur.com/gallery/xMM2W
+//https://imgur.com/gallery/5yeBVeM
