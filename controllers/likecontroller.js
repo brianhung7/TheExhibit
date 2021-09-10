f0baad95e698558772c4c891c7462a0c99be9ac5
brianhung7 <brian.texts@gmail.com>
2021-09-10 11:17:43 -0700

    unlike logic check to limit unlikes

--- controllers/likecontroller.js
+++ controllers/likecontroller.js
@@ -38,25 +38,26 @@ router.put("/:id/unlike", async (req, res, next) => {
     try {
         const foundLikes = await Like.findOne({ post: req.params.id });
         //Checks if user already liked a post
-        // let userInArray = false;
-        let userLikeArrIndex = 0;
+        let userInArray = false;
+        // let userLikeArrIndex = 0;
         for (i = 0; i < foundLikes.userArr.length; i++) {
             if (foundLikes.userArr[i] == req.session.currentUser.id) {
-                // userInArray = true;
+                userInArray = true;
                 foundLikes.userArr.splice(i, 1);
             }
         }
-
-        foundLikes.numLikes--;
-        // foundLikes.userArr.push(req.session.currentUser.id);
-        await Like.findByIdAndUpdate(
-            foundLikes._id,
-            {
-                numLikes: foundLikes.numLikes,
-                userArr: foundLikes.userArr,
-            },
-            { new: true },
-        )
+        //runs only if user liked the post, can't spam it to keep unliking post
+        if (userInArray) {
+            foundLikes.numLikes--;
+            await Like.findByIdAndUpdate(
+                foundLikes._id,
+                {
+                    numLikes: foundLikes.numLikes,
+                    userArr: foundLikes.userArr,
+                },
+                { new: true },
+            )
+        }
 
         return res.redirect(`/gallery/${req.params.id}`);
     } catch (error) {
