f1d9ed35c8c7f7a0461d67c6f4c18c6dd7c33c14
brianhung7 <brian.texts@gmail.com>
2021-09-15 09:22:08 -0700

    heroku deployment setup

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 /* NOTE this is our db connection string */
 require('dotenv').config();
-// const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/ticketdiary';
+const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/theexhibit';
 
-const connectionStr = 'mongodb://localhost:27017/theexhibit';
+// const connectionStr = 'mongodb://localhost:27017/theexhibit';
 
 
 mongoose.connect(connectionStr, {
