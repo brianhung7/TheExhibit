2b6ec54cedf524f42a38bad3ff33245175908381
brianhung7 <brian.texts@gmail.com>
2021-09-15 17:23:30 -0700

    More tooltips on gallery view

--- utils/navlinks.js
+++ utils/navlinks.js
@@ -3,20 +3,20 @@ module.exports = function navLinks(req, res, next) {
     
     if (req.session.currentUser) {
         res.locals.routes = [
-            { href: "/gallery", title: "Gallery"},
-            { href: "/gallery/feed", title: "Feed"},
-            { href: "/gallery/museum/1", title: "Museum"},
-            { href: "/conversation", title: "Conversations" },
-            { href: "/users/cart", title: "Cart" },
-            { href: "/users/purchases", title: "Purchases" },
-            { href: "/users/sales", title: "Sales" },
-            { href: "/gallery/new", title: "New Post" },
-            { href: "/logout", title: "Logout" },
+            { href: "/gallery", title: "Gallery", tooltip: "View all artwork"},
+            { href: "/gallery/feed", title: "Feed", tooltip: "View only posts from users you are following"},
+            { href: "/gallery/museum/1", title: "Museum", tooltip: "View artwork that you can't interact with"},
+            { href: "/conversation", title: "Conversations",tooltip:"View your messages" },
+            { href: "/users/cart", title: "Cart", tooltip:"View your shopping cart" },
+            { href: "/users/purchases", title: "Purchases",tooltip:"View your purchases" },
+            { href: "/users/sales", title: "Sales",tooltip:"View your sales" },
+            { href: "/gallery/new", title: "New Post",tooltip:"Make a new post" },
+            { href: "/logout", title: "Logout",tooltip:"Please come back soon" },
         ];
     } else {
         res.locals.routes = [
-            { href: "/login", title: "Login" },
-            { href: "/register", title: "Register" },
+            { href: "/login", title: "Login", tooltip:"Login to an account" },
+            { href: "/register", title: "Register", tooltip:"Sign up for an account" },
         ];
     }
     /*
